b25c0c416ee72fb08f1b6cb193b167c6529b9180
bella <theboss@ciaobella.me>
2021-06-11 11:35:07 -0700

    first commit and success!

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I will find the area of your circle! Just give me a radius.");
+            var radius = double.Parse(Console.ReadLine());
+            double pi = Math.PI;
+            double Aree = (Math.PI*radius*2);
+            Console.WriteLine($"The Area of the circle be {Aree}");
+            
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
